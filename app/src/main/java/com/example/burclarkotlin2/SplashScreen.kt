e89b5f133976526fe36cfef42dfad6771e4b4a54
Tolga Budanoğlu <budanoglu.tolga@gmail.com>
2022-08-26 15:12:55 +0300

    Giriş ekranı eklendi ve uygulama bitti

---
+++ app/src/main/java/com/example/burclarkotlin2/SplashScreen.kt
@@ -0,0 +1,43 @@
+package com.example.burclarkotlin2
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        var logoDondurAnimasyon = AnimationUtils.loadAnimation(this,R.anim.logo_dondur)
+        imgLogo.animation = logoDondurAnimasyon
+
+
+    }
+
+    override fun onResume() {
+
+        object :CountDownTimer(3000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+                /*var intent = Intent(this@SplashScreen,MainActivity::class.java)
+                startActivity(intent)*/
+                val openStartingPoint = Intent(
+                    this@SplashScreen,
+                    MainActivity::class.java
+                ) //creating an intent which is basically a reference to the Android Manifest
+
+                startActivity(openStartingPoint)
+            }
+
+        }.start()
+        super.onResume()
+    }
+}
