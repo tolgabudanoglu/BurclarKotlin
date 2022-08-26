f02292acb8018811e4ea11b415ba0dbf50ac354e
Tolga Budanoğlu <budanoglu.tolga@gmail.com>
2022-08-26 14:08:04 +0300

    Palette Api eklendi

--- app/src/main/java/com/example/burclarkotlin2/DetayActivity.kt
+++ app/src/main/java/com/example/burclarkotlin2/DetayActivity.kt
@@ -1,7 +1,9 @@
 package com.example.burclarkotlin2
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.palette.graphics.Palette
 import kotlinx.android.synthetic.main.activity_detay.*
 
 class DetayActivity : AppCompatActivity() {
@@ -16,10 +18,28 @@ class DetayActivity : AppCompatActivity() {
         header.setImageResource(tumBurcBilgileri.get(position).burcBuyukResim)
 
         setSupportActionBar(anim_toolbar)
+        getSupportActionBar()!!.setDisplayHomeAsUpEnabled(true)
         collapsing_toolbar.title = tumBurcBilgileri.get(position).burcAdi
 
+        var bitmap = BitmapFactory.decodeResource(resources,tumBurcBilgileri.get(position).burcBuyukResim)
+
+        Palette.from(bitmap).generate(object : Palette.PaletteAsyncListener{
+            override fun onGenerated(palette: Palette?) {
+                var color = palette?.getVibrantColor(com.google.android.material.R.attr.colorAccent)
+                collapsing_toolbar.setContentScrimColor(color!!)
+
+                window.statusBarColor = color
+            }
+
+        })
 
 
 
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
